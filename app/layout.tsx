e43e6fe862c42751f1b5dd215187ab641b9faf6f
Angelika Mikołajczyk <angelika.mikolajczyk11@gmail.com>
2023-03-03 13:18:03 +0100

    crossorigin fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html>
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet" />
             </head>
             <body className="flex flex-col sm:flex-row">
